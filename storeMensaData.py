6edf1ead39cd4e1c3c0e6a7820bd3a0ba0cc077c
techware01 <Anonymus1404@gmail.com>
2020-03-07 16:57:26 +0100

    files are now saved to data dir.

--- storeMensaData.py
+++ storeMensaData.py
@@ -6,11 +6,6 @@ import csv
 API_URL = 'https://openmensa.org/api/v2'
 
 
-def getRequestUrl(canteen_id, day):
-    day_string = day.strftime('%Y-%m-%d')
-    return "{}/canteens/{}/days/{}/meals".format(API_URL, canteen_id, day_string)
-
-
 def getType(notes):
     print(notes)
     if('vegan' in notes):
@@ -25,6 +20,7 @@ def getPrice(price):
         return None
     return str(price).replace(".", ",")
 
+
 def isInterestingMeal(meal):
     for category in ['Angebot des Tages', 'Abendangebot']:
         if category == meal['category']:
@@ -34,30 +30,45 @@ def isInterestingMeal(meal):
     return True
 
 
+def getMealsFromApi(canteen_id, day):
+    day_string = day.strftime('%Y-%m-%d')
+    url = "{}/canteens/{}/days/{}/meals".format(
+        API_URL, canteen_id, day_string)
+    print(url)
+    resp = requests.get(url)
+    if(resp.status_code == 200):
+        return resp.json()
+    return []
+
+
+canteens = [[79, "alteMensa"], [78, "zeltschloesschen"],
+            [82, "siedepunkt"], [88, "bioMensa"]]
+
 if __name__ == "__main__":
-    sdate = date(2020, 1, 1)   # start date
-    edate = date(2020, 3, 6)   # end date
+    sdate = date(2019, 2, 5)   # start date
+    edate = date(2019, 12, 31)   # end date
 
     delta = edate - sdate       # as timedelta
 
-    results = []
-    with open('alteMensa.csv', 'wt') as f:
-        csv_writer = csv.writer(f, delimiter=';', dialect='excel')
-        # write head
-        csv_writer.writerow(['date', 'weekday', 'category',
-                             'name', 'employees', 'students', 'type'])  # write header
+    for canteen in canteens:
+        results = []
+        filename = 'data/{}_2019.csv'.format(canteen[1])
+        with open(filename, 'wt') as f:
+            csv_writer = csv.writer(f, delimiter=';', dialect='excel')
+            # write head
+            csv_writer.writerow(['date', 'weekday', 'category',
+                                 'name', 'employees', 'students', 'type'])  # write header
 
-        for i in range(delta.days + 1):
-            day = sdate + timedelta(days=i)
-            url = getRequestUrl(79, day)
-            print(url)
-            resp = requests.get(url)
-            meals = resp.json()
-            if meals != []:
-                for meal in meals:
-                    if (isInterestingMeal(meal)):
-                        employees_price = getPrice(meal['prices']['employees'])
-                        stundents_price = getPrice(meal['prices']['students'])
-                        row = [day.strftime('%Y-%m-%d'), day.strftime('%a'), meal['category'], meal['name'],
-                            employees_price, stundents_price, getType(meal['notes'])]
-                        csv_writer.writerow(row)
+            for i in range(delta.days + 1):
+                day = sdate + timedelta(days=i)
+                meals = getMealsFromApi(canteen[0], day)
+                if meals != []:
+                    for meal in meals:
+                        if (isInterestingMeal(meal)):
+                            employees_price = getPrice(
+                                meal['prices']['employees'])
+                            stundents_price = getPrice(
+                                meal['prices']['students'])
+                            row = [day.strftime('%Y-%m-%d'), day.strftime('%a'), meal['category'], meal['name'],
+                                   employees_price, stundents_price, getType(meal['notes'])]
+                            csv_writer.writerow(row)
